687bd571277e9bd5d6e4a30408b472fd02349ba7
NGHIEP BUI <nghiepbv2@fpt.com.vn>
2023-05-22 14:17:28 +0700

    fix: add gitgnore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,71 @@
 /* eslint-disable import/no-named-as-default-member */
-import { PrimaryLayout } from '@/layouts'
-import { NextPageWithLayout } from '@/models'
-import dynamic from 'next/dynamic'
-import { ReactElement } from 'react'
+import { fetchDataHome } from "@/api-client";
+import Home from "@/components/home";
+import { PrimaryLayout } from "@/layouts";
+import { Box, Stack, Typography } from "@mui/material";
+import { GetStaticProps, GetStaticPropsContext } from "next";
+import Image from "next/image";
+import { ReactElement } from "react";
 
-const Home = dynamic(() => import('../src/components/home'), {
-  ssr: false,
-})
-
-const HomePage: NextPageWithLayout = () => {
-  return <Home />
+export interface IProps {
+  data: any[];
 }
 
-export default HomePage
+const HomePage = ({ data }: IProps) => {
+  console.log("data:", data);
+  return (
+    <>
+      <Home />
+      <Stack
+        direction={"row"}
+        sx={{
+          position: "relative",
+          aspectRatio: `calc(${data?.length ?? 0 / 1})`,
+          bgcolor: "#FFFFFF",
+          overflow: "hidden",
+          width: "100%",
+        }}
+      >
+        {data?.length &&
+          data.map((item, index) => {
+            return (
+              <Box
+                key={index}
+                flex={1}
+                position="relative"
+                sx={{ cursor: "pointer" }}
+              >
+                <Image src={item.imgUrl} layout="fill" alt="img" />
+
+                <Typography
+                  variant="h5"
+                  letterSpacing={"1px"}
+                  textAlign="center"
+                  position={"absolute"}
+                  sx={{ bottom: 40, left: 0, right: 0 }}
+                >
+                  {item.name}
+                </Typography>
+              </Box>
+            );
+          })}
+      </Stack>
+    </>
+  );
+};
+
+export default HomePage;
 HomePage.getLayout = function getLayout(page: ReactElement) {
-  return <PrimaryLayout>{page}</PrimaryLayout>
-}
+  return <PrimaryLayout>{page}</PrimaryLayout>;
+};
+
+export const getStaticProps: GetStaticProps<IProps> = async (
+  context: GetStaticPropsContext
+) => {
+  const result = await fetchDataHome("categories");
+  return {
+    props: {
+      data: result ?? [],
+    },
+  };
+};
